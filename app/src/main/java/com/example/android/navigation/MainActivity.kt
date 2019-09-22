fe00df77c218ee834caa1e0b38bb272cfa018484
Negative9 <nine4567@gmail.com>
2019-09-22 23:41:59 +0700

    final

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,10 +19,11 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
-
+private lateinit var drawerLayout: DrawerLayout
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +31,15 @@ class MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.myNavigationHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupWithNavController(binding.navView , navController)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavigationHostFragment)
 
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 
     // TODO (01) Create the new TitleFragment
